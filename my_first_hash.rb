c4c3ff0bd33ce09efd53394402ec27ce328baa8c
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 00:14:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {"pastas" => 3, "soups" => 6, "entrees" => 5, "appetizers" = 9}
+  my_hash = {"pastas" => 3, "soups" => 6, "entrees" => 5, "appetizers" => 9}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
 
